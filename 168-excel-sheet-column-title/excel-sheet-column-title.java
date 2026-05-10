0a47c4a0174b3f5b074a36829a2810d6f995b545
AYUSH YADAV <raosahab.ay1203@gmail.com>
2026-05-10 20:50:19 +0530

    Time: 4 ms (1.32%) | Memory: 42.5 MB (59.08%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String convertToTitle(int n) {
+        String ans = "";
+
+        while(n > 0) {
+
+            n--;
+
+            ans = (char)('A' + n % 26) + ans;
+
+            n /= 26;
+        }
+
+        return ans;
+    }
+}
