609f9926b6054ddc3dd7f058bc320caf4c254b35
AYUSH YADAV <raosahab.ay1203@gmail.com>
2026-06-19 07:39:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (6.14%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -1,12 +1,18 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-        int n=gain.length;
-        int[] res=new int[n+1];
-        res[0]=0;
-        int max=0;
-        for(int i=1;i<n+1;i++){
-            res[i]=res[i-1]+gain[i-1];
-            max=Math.max(max,res[i]);
+        // int n=gain.length;
+        // int[] res=new int[n+1];
+        // res[0]=0;
+        // int max=0;
+        // for(int i=1;i<n+1;i++){
+        //     res[i]=res[i-1]+gain[i-1];
+        //     max=Math.max(max,res[i]);
+        // }
+        // return max;
+        int cur=0,max=0;
+        for(int g:gain){
+            cur+=g;
+            max=Math.max(max,cur);
         }
         return max;
     }
