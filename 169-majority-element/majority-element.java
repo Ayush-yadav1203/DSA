0a49bc279b0fa6e774a54d5cc587a1b830b64f68
AYUSH YADAV <raosahab.ay1203@gmail.com>
2026-07-06 13:09:36 +0530

    Time: 1 ms (99.89%) | Memory: 55.6 MB (59.55%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        // Arrays.sort(nums);
+        // return nums[nums.length/2];
+        int val=0;
+        int count=0;
+        for(int num:nums){
+            if(count==0){
+                val=num;
+            }
+            if(num==val){
+                count++;
+            }
+            else 
+                count--;
+        }
+        return val;
+    }
+}
