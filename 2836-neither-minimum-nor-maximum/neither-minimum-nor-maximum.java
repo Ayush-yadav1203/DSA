6b9578e2aaa61802bdf5566487d62e14f327f918
AYUSH YADAV <raosahab.ay1203@gmail.com>
2026-07-13 22:36:02 +0530

    Time: 4 ms (100.00%) | Memory: 47 MB (74.87%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
@@ -6,20 +6,27 @@ class Solution {
         //     return nums[n];
         // }
         // return -1;
-        int n=nums.length;
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MIN_VALUE;
-        if(n<=2) return -1;
-        for(int num:nums){
-            min=Math.min(min,num);
-            max=Math.max(max,num);
-        }
-        for(int num:nums){
-            if(num!=min && num!=max){
-                return num;
-            }
 
-        }
-        return -1;
+        // int n=nums.length;
+        // int min=Integer.MAX_VALUE;
+        // int max=Integer.MIN_VALUE;
+        // if(n<=2) return -1;
+        // for(int num:nums){
+        //     min=Math.min(min,num);
+        //     max=Math.max(max,num);
+        // }
+        // for(int num:nums){
+        //     if(num!=min && num!=max){
+        //         return num;
+        //     }
+
+        // }
+        // return -1;
+
+        if(nums.length<=2) return -1;
+        int a=nums[0];
+        int b=nums[1];
+        int c=nums[2];
+        return a+b+c-Math.min(a,Math.min(b,c))-Math.max(a,Math.max(b,c));
     }
 }
