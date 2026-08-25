4a0e09448ad0e081b47e258111f9e34753571181
AYUSH YADAV <raosahab.ay1203@gmail.com>
2026-08-25 13:02:54 +0530

    Time: 3 ms (99.82%) | Memory: 94.4 MB (67.52%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,20 +10,58 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        List<Integer> list=new ArrayList<>();
+        // List<Integer> list=new ArrayList<>();
         
-        while(head!=null){
-            list.add(head.val);
-            head=head.next;
+        // while(head!=null){
+        //     list.add(head.val);
+        //     head=head.next;
+        // }
+        // int l=0,r=list.size()-1;
+        // while(l<r){
+        //     if(!list.get(l).equals(list.get(r))){
+        //         return false;
+        //     }
+        //     l++;
+        //     r--;
+        // }
+        // return true;
+
+        if (head == null || head.next == null) {
+            return true;
         }
-        int l=0,r=list.size()-1;
-        while(l<r){
-            if(!list.get(l).equals(list.get(r))){
+
+        // Find middle
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        ListNode prev = null;
+
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        // Compare first and reversed second half
+        ListNode left = head;
+        ListNode right = prev;
+
+        while (right != null) {
+            if (left.val != right.val) {
                 return false;
             }
-            l++;
-            r--;
+
+            left = left.next;
+            right = right.next;
         }
+
         return true;
     }
 }
